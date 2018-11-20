1206de6778ecf9e51f3bfcfb6241d84e11034937
Tobias Kunze <r@rixx.de>
2018-11-20 23:23:27 +0100

    Implement data disposal

--- src/c3queue.py
+++ src/c3queue.py
@@ -9,6 +9,7 @@ from dateutil import parser
 
 
 DATA_PATH = ''
+C3SECRET = os.environ.get('C3QUEUE_SECRET')
 
 
 @aiohttp_jinja2.template('stats.html')
@@ -17,8 +18,23 @@ async def stats(request):
     return {'data': data}
 
 
-def pong(request):
-    return aiohttp_jinja2.render_template('405.html', request, {})
+async def pong(request):
+    if not 'Authorization' in request.headers or request.headers['Authorization'] != C3SECRET:
+        return aiohttp_jinja2.render_template('405.html', request, {})
+    try:
+        data = await request.post()
+    except:
+        return aiohttp_jinja2.render_template('405.html', request, {})
+    if 'ping' in data and 'pong' in data:
+        try:
+            ping = parser.parse(data['ping'])
+            pong = parser.parse(data['pong'])
+        except:
+            return aiohttp_jinja2.render_template('405.html', request, {})
+        else:
+            await write_line(ping, pong)
+            return web.Response(status=201)
+    return aiohttp_jinja2.render_template('405.html', request, {'data': data})
 
 
 async def data(request):
@@ -30,16 +46,19 @@ async def data(request):
 async def parse_data():
     result = []
     async with aiofiles.open(DATA_PATH) as d:
-        async for row in csv.DictReader(d):
-            ping = parser.parse(row['ping'])
-            pong = parser.parse(row['pong'])
+        async for row in d:
+            if row.strip() == 'ping,pong':
+                continue
+            ping, pong = row.split(',')
+            ping = parser.parse(ping.strip('"'))
+            pong = parser.parse(pong.strip('"'))
             result.append({'ping': ping, 'pong': pong, 'rtt': (pong - ping) if (ping and pong) else None})
     return result
 
 
 async def write_line(ping, pong):
-    async with open(DATA_PATH, 'a') as d:
-        d.write('{},{}\n'.format(ping.isoformat(), pong.isoformat()))
+    async with aiofiles.open(DATA_PATH, 'a') as d:
+        await d.write('{},{}\n'.format(ping.isoformat(), pong.isoformat()))
 
 
 async def get_data_path():
@@ -53,7 +72,7 @@ async def get_data_path():
 async def main(argv):
     app = web.Application()
     app.add_routes([web.get('/', stats)])
-    app.add_routes([web.get('/pong', pong)])
+    app.add_routes([web.post('/pong', pong)])
     app.add_routes([web.get('/data', data)])
     aiohttp_jinja2.setup(app, loader=jinja2.PackageLoader('c3queue', 'templates'))
     await get_data_path()
